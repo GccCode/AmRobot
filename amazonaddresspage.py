192c4992b01195d46a696823eb05c19080c999a3
gcccode <gcccode@gmail.com>
2018-08-29 22:39:26 +0800

    fix...

--- amazonaddresspage.py
+++ amazonaddresspage.py
@@ -37,38 +37,35 @@ class AmazonAddressPage(AmazonPage):
     def fill_in_form(self, addresstype, begin, end):
         country = self.cf.get("account", "country")
         if addresstype == "bill":
+            if country == "jp":
+                self.fill_in_form_jp("bill", begin, end)
+            elif country == "us":
+                self.fill_in_form_us("bill", begin, end)
+        elif country == "fba":
+            if country == "jp":
+                self.fill_in_form_jp("fba", begin, end)
+            elif country == "us":
+                self.fill_in_form_us("fba", begin, end)
+
+    def fill_in_form_jp(self, addresstype, begin, end):
+        if addresstype == "bill":
             fullname = self.cf.get("bill_address", "fullname")
             line1 = self.cf.get("bill_address", "addressline1")
             phonenumber = self.cf.get("bill_address", "phone")
-            if country == "us":
-                city = self.cf.get("bill_address", "city")
-                state = self.cf.get("bill_address", "state")
-                postalcode = self.cf.get("bill_address", "postalcode")
-            elif country == "jp":
-                city = self.cf.get("bill_address", "city")
-                state = self.cf.get("bill_address", "state")
-                postalcode = self.cf.get("bill_address", "postalcode")
-                # state = self.cf.get("bill_address", "state")
-                # state_index = self.locator_state_jp(state)
-                # line2 = self.cf.get("bill_address", "addressline2")
-                # postalcode1 = self.cf.get("bill_address", "postalcode1")
-                # postalcode2 = self.cf.get("bill_address", "postalcode2")
+            city = self.cf.get("bill_address", "city")
+            state = self.cf.get("bill_address", "state")
+            postalcode = self.cf.get("bill_address", "postalcode")
         elif addresstype == "fba":
             fullname = self.cf.get("fba_address", "fullname")
             line1 = self.cf.get("fba_address", "addressline1")
             phonenumber = self.cf.get("fba_address", "phone")
-            if country == "us":
-                city = self.cf.get("fba_address", "city")
-                state = self.cf.get("fba_address", "state")
-                postalcode = self.cf.get("fba_address", "postalcode")
-            elif country == "jp":
-                state = self.cf.get("fba_address", "state")
-                state_index = self.locator_state_jp(state)
-                line2 = self.cf.get("fba_address", "addressline2")
-                postalcode1 = self.cf.get("fba_address", "postalcode1")
-                postalcode2 = self.cf.get("fba_address", "postalcode2")
+            state = self.cf.get("fba_address", "state")
+            state_index = self.locator_state_jp(state)
+            line2 = self.cf.get("fba_address", "addressline2")
+            postalcode1 = self.cf.get("fba_address", "postalcode1")
+            postalcode2 = self.cf.get("fba_address", "postalcode2")
 
-        if country == "jp" and addresstype == "bill":
+        if addresstype == "bill":
             self.select(self.locator.UNITEDSTATEINDEX, *self.locator.COUNTRYSELECT)
             self.random_sleep(1000, 2000)
 
@@ -82,13 +79,13 @@ class AmazonAddressPage(AmazonPage):
         self.input(line1, *self.locator.ADDRESSLINE1)
         self.random_sleep(1000, 2000)
 
-        if country == "jp" and addresstype == "fba":
+        if addresstype == "fba":
             self.click(*self.locator.ADDRESSLINE2)
             self.random_sleep(1000, 2000)
             self.input(line2, *self.locator.ADDRESSLINE2)
             self.random_sleep(1000, 2000)
 
-        if country == "us" or (country == "jp" and addresstype == "bill"):
+        if addresstype == "bill":
             self.click(*self.locator.ADDRESSCITY)
             self.random_sleep(1000, 2000)
             self.input(city, *self.locator.ADDRESSCITY)
@@ -103,7 +100,7 @@ class AmazonAddressPage(AmazonPage):
             self.random_sleep(1000, 2000)
             self.input(postalcode, *self.locator.ADDRESSPOSTALCODE)
             self.random_sleep(1000, 2000)
-        elif country == "jp" and addresstype == "fba":
+        elif addresstype == "fba":
             self.click(*self.locator.ADDRESSPOSTALCODEONE)
             self.random_sleep(1000, 2000)
             self.input(postalcode1, *self.locator.ADDRESSPOSTALCODEONE)
@@ -125,6 +122,148 @@ class AmazonAddressPage(AmazonPage):
         self.click(*self.locator.ADDADDRESSSUBMIT)
         self.random_sleep(begin, end)
         self.wait_page_loaded(*self.locator.ADDADDRESS)
+
+    def fill_in_form_us(self, addresstype, begin, end):
+        if addresstype == "bill":
+            fullname = self.cf.get("bill_address", "fullname")
+            line1 = self.cf.get("bill_address", "addressline1")
+            phonenumber = self.cf.get("bill_address", "phone")
+            city = self.cf.get("bill_address", "city")
+            state = self.cf.get("bill_address", "state")
+            postalcode = self.cf.get("bill_address", "postalcode")
+        elif addresstype == "fba":
+            fullname = self.cf.get("fba_address", "fullname")
+            line1 = self.cf.get("fba_address", "addressline1")
+            phonenumber = self.cf.get("fba_address", "phone")
+            city = self.cf.get("fba_address", "city")
+            state = self.cf.get("fba_address", "state")
+            postalcode = self.cf.get("fba_address", "postalcode")
+
+        self.click(*self.locator.FULLNAME)
+        self.random_sleep(1000, 2000)
+        self.input(fullname, *self.locator.FULLNAME)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDRESSLINE1)
+        self.random_sleep(1000, 2000)
+        self.input(line1, *self.locator.ADDRESSLINE1)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDRESSCITY)
+        self.random_sleep(1000, 2000)
+        self.input(city, *self.locator.ADDRESSCITY)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDRESSSTATE)
+        self.random_sleep(1000, 2000)
+        self.input(state, *self.locator.ADDRESSSTATE)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDRESSPOSTALCODE)
+        self.random_sleep(1000, 2000)
+        self.input(postalcode, *self.locator.ADDRESSPOSTALCODE)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDRESSPHONE)
+        self.random_sleep(1000, 2000)
+        self.input(phonenumber, *self.locator.ADDRESSPHONE)
+        self.random_sleep(1000, 2000)
+
+        self.click(*self.locator.ADDADDRESSSUBMIT)
+        self.random_sleep(begin, end)
+        self.wait_page_loaded(*self.locator.ADDADDRESS)
+
+    # def fill_in_form(self, addresstype, begin, end):
+    #     country = self.cf.get("account", "country")
+    #     if addresstype == "bill":
+    #         fullname = self.cf.get("bill_address", "fullname")
+    #         line1 = self.cf.get("bill_address", "addressline1")
+    #         phonenumber = self.cf.get("bill_address", "phone")
+    #         if country == "us":
+    #             city = self.cf.get("bill_address", "city")
+    #             state = self.cf.get("bill_address", "state")
+    #             postalcode = self.cf.get("bill_address", "postalcode")
+    #         elif country == "jp":
+    #             city = self.cf.get("bill_address", "city")
+    #             state = self.cf.get("bill_address", "state")
+    #             postalcode = self.cf.get("bill_address", "postalcode")
+    #             # state = self.cf.get("bill_address", "state")
+    #             # state_index = self.locator_state_jp(state)
+    #             # line2 = self.cf.get("bill_address", "addressline2")
+    #             # postalcode1 = self.cf.get("bill_address", "postalcode1")
+    #             # postalcode2 = self.cf.get("bill_address", "postalcode2")
+    #     elif addresstype == "fba":
+    #         fullname = self.cf.get("fba_address", "fullname")
+    #         line1 = self.cf.get("fba_address", "addressline1")
+    #         phonenumber = self.cf.get("fba_address", "phone")
+    #         if country == "us":
+    #             city = self.cf.get("fba_address", "city")
+    #             state = self.cf.get("fba_address", "state")
+    #             postalcode = self.cf.get("fba_address", "postalcode")
+    #         elif country == "jp":
+    #             state = self.cf.get("fba_address", "state")
+    #             state_index = self.locator_state_jp(state)
+    #             line2 = self.cf.get("fba_address", "addressline2")
+    #             postalcode1 = self.cf.get("fba_address", "postalcode1")
+    #             postalcode2 = self.cf.get("fba_address", "postalcode2")
+    #
+    #     if country == "jp" and addresstype == "bill":
+    #         self.select(self.locator.UNITEDSTATEINDEX, *self.locator.COUNTRYSELECT)
+    #         self.random_sleep(1000, 2000)
+    #
+    #     self.click(*self.locator.FULLNAME)
+    #     self.random_sleep(1000, 2000)
+    #     self.input(fullname, *self.locator.FULLNAME)
+    #     self.random_sleep(1000, 2000)
+    #
+    #     self.click(*self.locator.ADDRESSLINE1)
+    #     self.random_sleep(1000, 2000)
+    #     self.input(line1, *self.locator.ADDRESSLINE1)
+    #     self.random_sleep(1000, 2000)
+    #
+    #     if country == "jp" and addresstype == "fba":
+    #         self.click(*self.locator.ADDRESSLINE2)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(line2, *self.locator.ADDRESSLINE2)
+    #         self.random_sleep(1000, 2000)
+    #
+    #     if country == "us" or (country == "jp" and addresstype == "bill"):
+    #         self.click(*self.locator.ADDRESSCITY)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(city, *self.locator.ADDRESSCITY)
+    #         self.random_sleep(1000, 2000)
+    #
+    #         self.click(*self.locator.ADDRESSSTATE)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(state, *self.locator.ADDRESSSTATE)
+    #         self.random_sleep(1000, 2000)
+    #
+    #         self.click(*self.locator.ADDRESSPOSTALCODE)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(postalcode, *self.locator.ADDRESSPOSTALCODE)
+    #         self.random_sleep(1000, 2000)
+    #     elif country == "jp" and addresstype == "fba":
+    #         self.click(*self.locator.ADDRESSPOSTALCODEONE)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(postalcode1, *self.locator.ADDRESSPOSTALCODEONE)
+    #         self.random_sleep(1000, 2000)
+    #
+    #         self.click(*self.locator.ADDRESSPOSTALCODETWO)
+    #         self.random_sleep(1000, 2000)
+    #         self.input(postalcode2, *self.locator.ADDRESSPOSTALCODETWO)
+    #         self.random_sleep(1000, 2000)
+    #
+    #         self.select(state_index, *self.locator.ADDRESSSTATESELECT)
+    #         self.random_sleep(1000, 2000)
+    #
+    #     self.click(*self.locator.ADDRESSPHONE)
+    #     self.random_sleep(1000, 2000)
+    #     self.input(phonenumber, *self.locator.ADDRESSPHONE)
+    #     self.random_sleep(1000, 2000)
+    #
+    #     self.click(*self.locator.ADDADDRESSSUBMIT)
+    #     self.random_sleep(begin, end)
+    #     self.wait_page_loaded(*self.locator.ADDADDRESS)
 
 
 # if __name__ == "__main__":
