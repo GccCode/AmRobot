cb39140744be4ec6d2364e2d808134e05ab803b2
gcccode <gcccode@gmail.com>
2018-09-02 21:28:08 +0800

    fix...

--- amrobot.py
+++ amrobot.py
@@ -48,9 +48,9 @@ def customized_broswer():
     status = cf.get("proxy", "status")
     if status == "1":
         host_port = cf.get("proxy", "proxy")
-        ua = cf.get("broswer", "type")
         proxy_socks_argument = '--proxy-server=socks5://' + host_port
         option.add_argument(proxy_socks_argument)
+    ua = cf.get("broswer", "type")
     if ua == "2":
         option.add_argument(
             '--user-agent=Mozilla/5.0 (Windows NT 6.1; rv:61.0) Gecko/20100101 Firefox/61.0')
