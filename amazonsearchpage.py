e3c05c80a1931d8ec06d0d8c8ed707ed5b50b18b
gcccode <gcccode@gmail.com>
2018-09-03 09:15:04 +0800

    fix...

--- amazonsearchpage.py
+++ amazonsearchpage.py
@@ -43,8 +43,8 @@ class AmazonSearchPage(AmazonPage):
         if asin == False:
             tmp = random.randint(0, (len(asinresults) - 1))
             currenthandle = self.enter_asin_page(asinresults[tmp], asinresults[tmp].get_attribute('data-asin'), 3000, 5000)
-            random_status = random.randint(1, 2)
-            if random_status == 2:
+            random_status = random.randint(1, 200)
+            if (random_status % 2) == 1:
                 self.random_walk(count)
             self.back_prev_page_by_country(currenthandle, begin, end)
             print("访问当前页面任意产品。。。\n")
@@ -58,8 +58,8 @@ class AmazonSearchPage(AmazonPage):
                         tmp = random.randint(0, (len(asinresults) - 1))
 
                     currenthandle = self.enter_asin_page(asinresults[tmp], asinresults[tmp].get_attribute('data-asin'), 3000, 5000)
-                    random_status = random.randint(1, 2)
-                    if random_status == 2:
+                    random_status = random.randint(1, 200)
+                    if (random_status % 2) == 1:
                         self.random_walk(count)
                     self.back_prev_page_by_country(currenthandle, begin, end)
                     break
