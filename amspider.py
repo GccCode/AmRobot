3b5c266776fd0df550a579e094f6e1e7ce90fef0
gcccode <gcccode@gmail.com>
2018-09-19 15:31:56 +0800

    fix...

--- amspider.py
+++ amspider.py
@@ -57,7 +57,7 @@ def jp_node_gather():
         amazonpage.random_sleep(3000, 5000)
         print("Start gathering....", flush=True)
         if amazonpage.is_element_exsist(*TOP3_CONTAINER):
-            for i in range(0, 3):
+            for i in range(1, 3):
                 tmp_symbol = TOP3_PRICE_PREFIX + str(i) + TOP3_PRICE_POSTFIX
                 element = driver.find_element_by_xpath(tmp_symbol)
                 print(element.text, flush=True)
