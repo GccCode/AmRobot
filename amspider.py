1706cb1d14ea6967b46ba93837b28c0480f0cfcc
gcccode <gcccode@gmail.com>
2018-09-19 10:08:28 +0800

    fix...

---
+++ amspider.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from selenium import webdriver
+import re
+from selenium.webdriver.common.by import By
+from amazonasinpage import AmazonAsinPage
+import time
+
+
+item_prefix = "//*[@id='zg-ordered-list']/li[position()="
+item_postfix = "]/span"
+price_symbol = ".//div/span/div[position()=2]/a[position()=1]/span/span"
+review_symbol = ".//div/span/div[position()=1]/a[position()=2]"
+href_symbol = ".//div/span/div[position()=2]/a[position()=1]"
+rate_symbol = ".//div/span/div[position()=1]/a"
+BUYER_COUNT = (By.XPATH, "//*[@id='olp_feature_div']/div/span[position()=1]/a")
+QA_COUNT = (By.ID, "askATFLink")
+QA_COUNT1 = (By.XPATH, "//*[@id='askATFLink']/span")
+FBA_FLAG = (By.ID, "SSOFpopoverLink")
+
+def getasinfromhref(template):
+    rule = r'dp/(.*?)/ref'
+    slotList = re.findall(rule, template)
+    return slotList[0]
+
+def test_node_gather():
+    driver = webdriver.Chrome()
+    driver.set_page_load_timeout(60)
+    driver.set_script_timeout(60)
+    try:
+        driver.get("https://www.amazon.com/gp/bestsellers/electronics/297859")
+        for i in range(1, 50):
+            item_symbol = item_prefix + str(i) + item_postfix
+            element = driver.find_element_by_xpath(item_symbol)
+            price = element.find_element_by_xpath(price_symbol)
+            price_text = price.text
+            href = element.find_element_by_xpath(href_symbol)
+            asin_text = getasinfromhref(href.get_attribute("href"))
+            review = element.find_element_by_xpath(review_symbol)
+            review_text = review.text
+            rate = element.find_element_by_xpath(rate_symbol)
+            rate_text = rate.get_attribute("title").split(" ")[0]
+            tmp = asin_text + " " + price_text.strip('$') + " " + review_text.replace(',', '') + " " + rate_text
+            print(tmp, flush=True)
+    except:
+        print("xxxxxxx")
+    finally:
+        driver.quit()
+
+def test_get_inventory():
+    driver = webdriver.Chrome()
+    driver.set_page_load_timeout(60)
+    driver.set_script_timeout(60)
+    try:
+        driver.get("https://www.amazon.com/dp/B078H7VY19")
+        amazonasinpage = AmazonAsinPage(driver)
+        if amazonasinpage.is_element_exsist(FBA_FLAG):
+            print("product is fba...", flush=True)
+        else:
+            print("product is fbm or not exsist...", flush=True)
+        if amazonasinpage.is_element_exsist(QA_COUNT):
+            element = driver.find_element_by_id("askATFLink")
+            element.find_element_by_xpath(".//span")
+            print("aaa")
+            print(element.text)
+        else:
+            print("qa_count not exsist...", flush=True)
+
+        amazonasinpage.add_cart(5000, 8000)
+    except:
+        print("xxxx")
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    test_get_inventory()
